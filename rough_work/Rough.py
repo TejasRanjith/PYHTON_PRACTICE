a6d5ea8fed2a5d09b6dcf5fc9d2687da0a06ace4
Tejas <tejasranjith035611@gmail.com>
2021-01-24 16:55:48 +0400

    M(Rough.py)

--- rough_work/Rough.py
+++ rough_work/Rough.py
@@ -2,9 +2,16 @@
 #os.system("python grp_app//Bank_Management_System.py")
 #os.system("python Rough.py")
 
-#to run another python file in this file ↑↑↑
+#to run another python file in this file. ↑↑↑
 
-import re
-s = "blahblah@yahoo.com"
-domain = re.search("@[\w.]+", s)
-print (domain.group())
+# import re
+# s = "blahblah@yahoo.com"
+# domain = re.search("@[\w.]+", s)
+# print (domain.group())
+
+#to extract the domain part of an email. ↑↑↑
+
+#print(bin(-1))
+#it returns -0b1
+import os
+os.system("cmd")
